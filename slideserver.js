bc355ab120351babb2a5bb655a4c9f1e06a28858
Arnaud Morin <arnaud.morin@gmail.com>
2012-05-11 18:15:23 +0200

    Update nodejs slideserver.js with better client gestion
    slideserver.js has been updated to take care of clients connection.
    A login has been added also to have a more friendly username.
    
    template.html has been updated to work with websocket server.
    This version works pretty well in my environement but is not yet
    fully ready.
    
    shells/prez.html has been created based on embedder.html but with
    functions to show current users watching the slides (left part of
    the page)

--- slideserver.js
+++ slideserver.js
@@ -1,87 +1,103 @@
 #!/usr/bin/env node
- 
-var WebSocketServer = require('./node_modules/websocket/lib/WebSocketServer');
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var connectionArray = new Array();
+// Open socket
+var io = require('socket.io').listen(80);
+io.set('log level', 1); // reduce logging
+
+var debug = true;
+
+// Store the clients already connected in an array
+var socketArray = new Array();
+
+// superConnectionID will be the guy controlling the presentation
 var superConnectionID = null;
+
+// Initiate IDs
 var nextID = Date.now();
-var appendToMakeUnique = 1;
- 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + " Received request for " + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + " Server is listening on port 8080");
-});
- 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: true
-});
- 
-function getConnectionForID(id) {
-  var connect = null;
- 
-  for (i=0; i<connectionArray.length; i++) {
-    if (connectionArray[i].clientID == id) {
-      connect = connectionArray[i];
-      break;
-    }
-  }
- 
-  return connect;
-}
- 
-wsServer.on('connect', function(connection) {
-    console.log((new Date()) + " Connection accepted.");     
-    console.log("connectionArray.length: " + connectionArray.length);
-    if (connectionArray.length == 0){
+
+// Wait for connection
+io.sockets.on('connection', function(socket){
+    // If this is the first client connecting, he will be the controlling guy
+    if (socketArray.length == 0){
         superConnectionID = nextID;
-        console.log("superConnectionID: " + superConnectionID);
     }
-    connectionArray.push(connection);
-   
-    // Send the new client its token; it will
-    // respond with its login username.
-   
-    connection.clientID = nextID;
+    
+    // Store this client
+    socketArray.push(socket);
+    
+    // Set ID
+    socket.clientID = nextID;
+    socket.login = "unknown";
+    
+    // Compute next ID
     nextID++;
-   
-    var msg = {
-      type: "id",
-      id: connection.clientID
-    };
-    connection.sendUTF(JSON.stringify(msg));
-   
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received Message: " + message.utf8Data);
-           
-            // Process messages
-            var sendToClients = false;
-            var msg = JSON.parse(message.utf8Data);
-            if (msg.id == superConnectionID)
-                sendToClients = true;
-            var connect = getConnectionForID(msg.id);
-           
-            // Convert the message back to JSON and send it.
-            if (sendToClients) {
-              var msgString = JSON.stringify(msg);
-             
-              for (i=0; i<connectionArray.length; i++) {
-                connectionArray[i].sendUTF(msgString);
-              }
+    
+    // And your ID is ...
+    var msg = {type: 'id', id: socket.clientID};
+    socket.send(JSON.stringify(msg));
+
+    if (debug) console.log("New client: " + msg.id);
+    
+    // Wait for message from client
+    socket.on('message', function(message) {
+        // Process messages
+        var sendToClients = false;
+        var msg = JSON.parse(message);
+
+        if (debug) console.log(socket.login + "(" + msg.id + ") > " + msg.data);
+
+        // If this is a message to ask to be presenter
+        if (msg.data == "MASTER")
+            superConnectionID = msg.id;
+
+        // If this is the controlling guy
+        if (msg.id == superConnectionID)
+            sendToClients = true;
+
+        // If this is a message to get clients list
+        if (msg.data == "LIST"){
+            sendList();
+            sendToClients = false;
+        }
+
+        if (msg.data.login){
+            if (debug) console.log("Update login for " + msg.id + " to " + msg.data.login);
+            socket.login = msg.data.login;
+            sendList();
+            sendToClients = false;
+        }
+
+
+        // Convert the message back to JSON and send it.
+        if (sendToClients) {
+            if (debug) console.log("        This message is from MASTER! It's important! Send it back to all clients");
+            msg = {type: 'data', data: msg.data};
+            var msgString = JSON.stringify(msg);
+            // Send to all clients, controlling guy included
+            for(i=0;i<socketArray.length;i++){
+                socketArray[i].send(msgString);
             }
         }
     });
-    connection.on('close', function(connection) {
-      connectionArray = connectionArray.filter(function(el, idx, ar) {
-              return el.connected;
-      });
-      console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+
+    socket.on('disconnect', function(){
+        // Remove this client from array
+        for(i=0;i<socketArray.length;i++){
+            if (socketArray[i].clientID == socket.clientID)
+                socketArray.splice(i,1);
+        }
+        sendList();
     });
-}); 
+});
+
+function sendList(){
+    var list = new Array();
+    for(i=0;i<socketArray.length;i++){
+        list[i] = socketArray[i].login;
+    }
+    msg = {type: 'list', data: "LIST", list: list};
+    var msgString = JSON.stringify(msg);
+    for(i=0;i<socketArray.length;i++){
+      socketArray[i].send(msgString);
+    }
+    if (debug) console.log("List sent: " + list);
+}
